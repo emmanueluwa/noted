52b7a5e86e2458628d1b55d6b23ee180277ddb8e
emmanueluwa <emmanuel@fulodev.com>
2024-06-28 10:34:10 +0100

    adding ability to create new notes from frontend

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
 import express from "express";
 import "./db";
 import noteRouter from "./routers/note";
+import cors from "cors";
 
 //create server
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.post("/", (req, res) => {
-  console.log(req.body);
-  res.json({ message: "Ich listening" });
-});
 
 app.use("/note", noteRouter);
 
